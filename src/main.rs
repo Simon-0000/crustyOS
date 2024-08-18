d1e2b467acfbcba0f590e635be1fb6617edd4f9b
Simon-0000 <asmarsimon@hotmail.com>
2024-08-18 11:16:49 -0400

    hello world! OS

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,15 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start()-> ! {
-
+    let message = b"Hello world!";
+    let buffer_address = 0xb8000 as *mut u8;
+    for(i,&c) in message.iter().enumerate(){
+        unsafe{
+            *buffer_address.offset(i as isize * 2) = c;
+            *buffer_address.offset(i as isize * 2 + 1) = 0xb;
+            
+        }
+    }
     loop{}
 }
 
