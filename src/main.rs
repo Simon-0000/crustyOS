372c1d173e721082611aeee79fca22fb171cbc11
Simon-0000 <asmarsimon@hotmail.com>
2024-08-25 12:34:38 +0000

    added exit and exit codes

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,24 @@
 use core::panic::PanicInfo;
 use core::fmt::Write;
 use vga_buffer::{Color, ColorCode, WRITER};
+use x86_64::instructions::port::Port;
+mod vga_buffer;
 
+const DEVICE_ADDRESS: u16= 0xf4;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+enum ExitCode{
+    Success = 0x10,
+    Failed = 0x11
+}
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
     println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+    Exit(ExitCode::Success);
 }
 
 #[test_case]
@@ -26,22 +36,34 @@ fn testing(){
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     cprintln!(Color::Red,"\n{}",info);
+    Exit(ExitCode::Failed);
     loop {}
 }
-
-mod vga_buffer;
+fn Exit(code : ExitCode)
+{
+    unsafe{
+        let mut port = Port::new(DEVICE_ADDRESS);
+        port.write(code as u32);
+    }
+}
 
 #[no_mangle]
 pub extern "C" fn _start()-> ! {
+    // let port =
+
     #[cfg(test)]
     {
         test_main();
-        loop{}
     }
-    println!("okkok{}",3+5);
-    print!("Hello");
-    print!(" world");
-    panic!("Some panic message");
+    #[cfg(not(test))]
+    {
+        println!("running...");
+        Exit(ExitCode::Success);
+    }
+
     loop{}
+
+
+
 }
 
